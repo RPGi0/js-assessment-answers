3f84e1f693ea5b9fc3027d5a9f9199695f0da44b
Rob <gio@llnnll.com>
2018-06-05 13:46:36 -0400

    completed async test suite

--- app/async.js
+++ app/async.js
@@ -1,11 +1,36 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
+  async: function (value) {
+    return new Promise(function(resolve, reject) {
+      var varType = typeof value;
 
+      // do some work, and then...
+      if (varType === 'boolean' || varType === 'string') resolve(value);
+      else reject('Value must be a string or boolean!');
+    });
   },
 
-  manipulateRemoteData: function(url) {
+  manipulateRemoteData: function (url) {
+    return new Promise(function(resolve) {
+      var xhr =  new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState !== 4) {
+          return;
+        }
 
+        if (xhr.status === 200) {
+          var responseJSON = JSON.parse(xhr.responseText);
+
+          var arr = _.map(responseJSON.people, function (obj) {
+            return obj.name;
+          });
+
+          resolve(arr.sort());
+        }
+      };
+      xhr.open('GET', url, true);
+      xhr.send();
+    });
   }
 };
