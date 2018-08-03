4027232de0f2a3364ed4afc31f09da35f3b10c4a
Rob <gio@llnnll.com>
2018-08-03 13:50:12 -0400

    completed modules section

--- app/modules.js
+++ app/modules.js
@@ -1,14 +1,16 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
-  createModule: function(str1, str2) {
-    let greeter = {
+  createModule: function (str1, str2) {
+    const greeter = {
       name: str2,
       greeting: str1
-    }
+    };
 
     function sayIt() {
-      return greeter.greeting + greeter.name;
+      return greeter.greeting + ", " + greeter.name;
     }
-    return sayIt();
+
+    return _.extend(greeter, {sayIt});
+  }
 };
