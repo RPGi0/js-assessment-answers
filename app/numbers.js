f4b26c047b6bf1a8e6afa11f7ba7002edab39b2d
Rob <gio@llnnll.com>
2018-08-06 16:41:19 -0400

    completed numbers section

--- app/numbers.js
+++ app/numbers.js
@@ -1,19 +1,37 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
-  valueAtBit: function(num, bit) {
-
+  valueAtBit: function (num, bit) {
+    const numToBinaryStr = parseInt(num, 10).toString(2);
+    return +numToBinaryStr.substr(-1 * bit, 1);
   },
 
-  base10: function(str) {
-
+  base10: function (str) {
+    return +parseInt(str, 2).toString(10);
   },
 
-  convertToBinary: function(num) {
-
+  convertToBinary: function (num) {
+    const numToBinaryStr = parseInt(num, 10).toString(2);
+    return ("00000000" + numToBinaryStr).substr(-8);
   },
 
-  multiply: function(a, b) {
+  multiply: function (a, b) {
+    function adjust(num) {
+      let exponent, multiplier;
 
+      if (num < 1) {
+        exponent = Math.floor(Math.log(num) * -1);
+        multiplier = Math.pow(10, exponent);
+
+        return {adjusted: num * multiplier, multiplier: multiplier};
+      }
+
+      return {adjusted: num, multiplier: 1};
+    }
+
+    a = adjust(a);
+    b = adjust(b);
+
+    return (a.adjusted * b.adjusted) / (a.multiplier * b.multiplier);
   }
 };
